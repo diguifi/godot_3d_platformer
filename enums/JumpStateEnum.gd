e8cf3affa2fc8ab33a079c9106e435547af84f75
diguifi <lumia-diguifi@hotmail.com>
2022-02-15 13:58:03 -0300

    Movement polish

---
+++ enums/JumpStateEnum.gd
@@ -0,0 +1,9 @@
+class_name JumpStateEnum
+
+enum {
+	GROUNDED,
+	JUMP,
+	DOUBLE_JUMP,
+	JUMP_FALL,
+	DOUBLE_JUMP_FALL
+}
--- player/Player.gd
+++ player/Player.gd
@@ -1,45 +1,47 @@
 extends KinematicBody
 
-enum JUMP_STATE {
-	GROUNDED,
-	JUMP,
-	DOUBLE_JUMP
-}
-
-export var has_double_jump = false
+# ------ variables ------
+export var coyote_timer = 0.05
+onready var graphics = $Graphics
 onready var anim_player = $Graphics/AnimationPlayer
+
+# constants
 const WALK_SPEED = 5
 const SPRINT_SPEED = 7
-const JUMP_FORCE = 12
+const JUMP_FORCE = 10.5
 const D_JUMP_FORCE = 8
 const GRAVITY = 24
 const MAX_FALL_SPEED = 40
-var y_velo = 0
-var facing_right = true
-var falling = false
-var just_jumped = false
-var walk_speed = 1.3
-var jump_state = JUMP_STATE.GROUNDED
 
+# movement
+var y_velo = -0.1
+var grounded = false
+var can_jump = false
+var colliding_right = false
+var colliding_left = false
+var just_jumped = false
+var jump_state = JumpStateEnum.GROUNDED
+
+# animation
+var facing_right = true
+var walk_anim_speed = 1.3
+
+# power ups
+var has_double_jump = false
+
+
+# ------ start and loop ------
 func _ready():
 	Signals.connect("get_power_up", self, "_get_power_up")
  
 func _physics_process(delta):
 	transform.origin.z = 0
-	var grounded = is_on_floor()
-	if grounded:
-		falling = false
-		jump_state = JUMP_STATE.GROUNDED
-
 	var move_dir = apply_movement()
-	apply_jump(grounded, delta)
+	apply_jump(delta)
 	apply_double_jump(delta)
 	check_fall()
-	play_animations(grounded, move_dir)
- 
-func flip():
-	$Graphics.rotation_degrees.y *= -1
-	facing_right = !facing_right
+	play_animations(move_dir)
+	check_grounded()
  
 func play_anim(anim, speed = 1):
 	if anim_player.current_animation == anim and anim_player.get_playing_speed() == speed:
@@ -48,48 +50,53 @@ func play_anim(anim, speed = 1):
 	anim_player.play(anim)
 
 func apply_movement():
-	walk_speed = 1.3
+	walk_anim_speed = 1.3
 	var move_dir = DirectionEnum.IDLE
 	var movement_modifier = WALK_SPEED
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") and !colliding_right:
 		move_dir = DirectionEnum.RIGHT
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") and !colliding_left:
 		move_dir = DirectionEnum.LEFT
 	if Input.is_action_pressed("sprint"):
-		walk_speed = 2
+		walk_anim_speed = 2
 		movement_modifier = SPRINT_SPEED
-	move_and_slide(Vector3(move_dir * movement_modifier, y_velo, 0), Vector3(0,1,0))
+	var move_result = move_and_slide(Vector3(move_dir * movement_modifier, y_velo, 0), Vector3(0,1,0))
+	y_velo = move_result.y
 	return move_dir
 	
-func apply_jump(grounded, delta):
+func apply_jump(delta):
 	just_jumped = false
 	y_velo -= GRAVITY * delta
 	if y_velo < -MAX_FALL_SPEED:
 		y_velo = -MAX_FALL_SPEED
-	if grounded:
+	if can_jump:
 		y_velo = -0.1
 		if Input.is_action_pressed("jump"):
 			y_velo = JUMP_FORCE
 			just_jumped = true
-			jump_state = JUMP_STATE.JUMP
+			jump_state = JumpStateEnum.JUMP
 	else:
 		if Input.is_action_just_released("jump"):
-			falling = true
+			if jump_state == JumpStateEnum.JUMP:
+				jump_state = JumpStateEnum.JUMP_FALL
+			elif jump_state == JumpStateEnum.DOUBLE_JUMP:
+				jump_state = JumpStateEnum.DOUBLE_JUMP_FALL
 
 func apply_double_jump(delta):
 	if Input.is_action_pressed("jump"):
-		if jump_state == JUMP_STATE.JUMP and has_double_jump and falling:
+		if jump_state == JumpStateEnum.JUMP_FALL and has_double_jump:
 			y_velo = D_JUMP_FORCE
 			just_jumped = true
-			jump_state = JUMP_STATE.DOUBLE_JUMP
-			falling = false
+			jump_state = JumpStateEnum.DOUBLE_JUMP
 	
 func check_fall():
+	var falling = jump_state == JumpStateEnum.JUMP_FALL or jump_state == JumpStateEnum.DOUBLE_JUMP_FALL
 	if falling:
 		if y_velo > 0:
 			y_velo = lerp(y_velo, 0, 0.15)
 
-func play_animations(grounded, move_dir):
+func play_animations(move_dir):
+	var falling = jump_state == JumpStateEnum.JUMP_FALL or jump_state == JumpStateEnum.DOUBLE_JUMP_FALL
 	if move_dir == DirectionEnum.LEFT and facing_right:
 		flip()
 	if move_dir == DirectionEnum.RIGHT and !facing_right:
@@ -97,13 +104,55 @@ func play_animations(grounded, move_dir):
 
 	if just_jumped:
 		play_anim("jump", 1.2)
+	elif !grounded and falling:
+		play_anim("fall")
 	elif grounded:
 		if move_dir == DirectionEnum.IDLE:
 			play_anim("idle")
 		else:
-			play_anim("walk", walk_speed)
+			play_anim("walk", walk_anim_speed)
+			
+func check_grounded():
+	if grounded:
+		can_jump = true
+		jump_state = JumpStateEnum.GROUNDED
+	if !grounded && can_jump:
+		coyote_time()
 
+func flip():
+	graphics.rotation_degrees.y *= -1
+	facing_right = !facing_right
+
+func coyote_time():
+	yield(get_tree().create_timer(coyote_timer),"timeout")
+	can_jump = false
+
+
+# ------ signals ------
 func _get_power_up(power):
 	if power == "double_jump":
 		has_double_jump = true
-	
+
+func _on_AreaRight_body_entered(body):
+	if body.name == "GridMap":
+		colliding_right = true
+
+func _on_AreaLeft_body_entered(body):
+	if body.name == "GridMap":
+		colliding_left = true
+		
+func _on_AreaRight_body_exited(body):
+	if body.name == "GridMap":
+		colliding_right = false
+
+func _on_AreaLeft_body_exited(body):
+	if body.name == "GridMap":
+		colliding_left = false
+
+func _on_AreaBottom_body_entered(body):
+	if body.name == "GridMap":
+		grounded = true
+
+func _on_AreaBottom_body_exited(body):
+	if body.name == "GridMap":
+		grounded = false
