264c2bb233f756940fc21f4c32315fd375461739
diguifi <lumia-diguifi@hotmail.com>
2022-02-15 21:03:43 -0300

    Fix camera position

--- camera/Camera.gd
+++ camera/Camera.gd
@@ -3,11 +3,12 @@ extends Camera
 onready var player = get_parent().get_node("Player")
 export var smooth_speed = 5.0
 export var offset: Vector3
+var offset_x = 4
 
 func _physics_process(delta):
 	if(player != null):
 		if player.facing_right:
-			offset.x = 2
+			offset.x = offset_x
 		else:
-			offset.x = -2
+			offset.x = -offset_x
 		translation = lerp(translation, player.translation + offset, smooth_speed * delta)
